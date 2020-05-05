a3dddf585f45b2b12c70b31ea7cfc66ac41f61bf
Min Lee <min.navy.lee@gmail.com>
2020-05-04 22:22:56 -0400

    admin page with access limit alert

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -70,5 +70,16 @@ class Admin extends Component {
     );
   }
 }
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    <p>
+      Restricted area! Only users with the admin role are authorized.
+    </p>
+  </div>
+);
 
-export default withFirebase(Admin);
+const condition = authUser =>
+  authUser && !!authUser.roles[ROLES.ADMIN];
+
+export default withAuthorization(condition)(AdminPage);
